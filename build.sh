9227d2303224219bf8099d4bb4552b4644901e1d
Kevin Weeks <kevinmichaelweeks@gmail.com>
2018-01-08 15:20:50 -0800

    Update on the website at Mon Jan  8 15:20:50 PST 2018

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Generate static files with buster
+buster generate --domain=http://127.0.0.1:2368
+
+# Generate rss feed
+mv static/rss/index.rss static/rss/index.html
+
+# Replace urls that were missed by buster
+find static/* -name robots.txt -type f -exec sed -i '' 's#http://localhost:2368#https://thecoffeecookbook.github.io#g' {} \;
+find static/* -name *.xsl -type f -exec sed -i '' 's#http://localhost:2368#https://thecoffeecookbook.github.io#g' {} \;
+find static/* -name *.xml -type f -exec sed -i '' 's#loc>http://localhost:2368#loc>https://thecoffeecookbook.github.io#g' {} \;
+find static/* -name *.html -type f -exec sed -i '' 's#u=http://localhost:2368#u=https://thecoffeecookbook.github.io#g' {} \;
+find static/* -name *.html -type f -exec sed -i '' 's#url=http://localhost:2368#url=https://thecoffeecookbook.github.io#g' {} \;
+find static/* -name *.html -type f -exec sed -i '' 's#href="http://localhost:2368#href="https://thecoffeecookbook.github.io#g' {} \;
+find static/* -name *.html -type f -exec sed -i '' 's#src="http://localhost:2368#src="https://thecoffeecookbook.github.io#g' {} \;
+find static/* -name *.html -type f -exec sed -i '' 's#link>http://localhost:2368#link>https://thecoffeecookbook.github.io#g' {} \;
+
+# Add CNAME file for github pages
+buster add-domain thecoffeecookbook.github.io
+
+# Copy files that were missed by buster
+cp humans.txt static/humans.txt
+cp -R content/images static/content
--- update_website.sh
+++ update_website.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-buster generate
+./build.sh
 git add -A
 git commit -m "Update on the website at $(date)"
-git push origin master
+git push
+git subtree push --prefix static origin deploy
